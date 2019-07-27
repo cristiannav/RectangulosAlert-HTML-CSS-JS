75e176a6066a053477e97923c7f92218a7c4deef
cristian <cristiannav.arg@gmail.com>
2019-07-27 02:06:06 -0300

    Se sube Ejercicio de Rectangulos

---
+++ js/alert.js
@@ -0,0 +1,14 @@
+function hacer_click1(){
+  alert("Div 1");
+  event.stopPropagation();
+}
+
+function hacer_click2(){
+  alert("Div 2");
+  event.stopPropagation();
+}
+
+function hacer_click3(){
+  alert("Div 3");
+  event.stopPropagation();
+}
